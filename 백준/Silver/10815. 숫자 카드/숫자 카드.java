f308f86db3ede2c6161c9698c4697fd9129fde23
chojuyeon94 <110347822+chojuyeon94@users.noreply.github.com>
2023-07-08 22:36:12 +0900

    [Silver V] Title: 숫자 카드, Time: 1272 ms, Memory: 104176 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < m; i++) {
+            if (binarySearch(Integer.parseInt(st.nextToken()))) {
+                bw.write("1 ");
+            } else {
+                bw.write("0 ");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static boolean binarySearch(int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == target) {
+                return true;
+            } else if (arr[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
+}
