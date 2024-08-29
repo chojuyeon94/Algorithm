4ad85c67cc71faf1db8d9badd69ff067092b67d5
chojuyeon94 <110347822+chojuyeon94@users.noreply.github.com>
2024-08-29 15:34:20 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 80 ms, Memory: 12460 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.kt
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.kt
@@ -1,29 +1,31 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
 fun main() {
-    val n = readLine()!!.toInt()
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val sb = StringBuilder()
+
+    val n = reader.readLine().toInt()
 
     for (i in 1..n) {
-        
-        for (j in 1..n-i) {
-            print(" ")
+        for (j in 1..n - i) {
+            sb.append(' ')
         }
-        
-        for (j in 1..2*i-1) {
-            print("*")
+        for (k in 1..2 * i - 1) {
+            sb.append('*')
         }
-        
-        println()
+        sb.append('\n')
     }
 
-    for (i in n-1 downTo 1) {
-
-        for (j in 1..n-i) {
-            print(" ")
+    for (i in n - 1 downTo 1) {
+        for (j in 1..n - i) {
+            sb.append(' ')
         }
-
-        for (j in 1..2*i-1) {
-            print("*")
+        for (k in 1..2 * i - 1) {
+            sb.append('*')
         }
-        
-        println()
+        sb.append('\n')
     }
+
+    println(sb.toString())
 }
