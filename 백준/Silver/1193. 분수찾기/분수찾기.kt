baba9c49f5d303cbc441d0e0296e0923303494b9
chojuyeon94 <110347822+chojuyeon94@users.noreply.github.com>
2024-09-12 16:44:47 +0900

    [Silver V] Title: 분수찾기, Time: 76 ms, Memory: 12260 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.kt
+++ 백준/Silver/1193. 분수찾기/분수찾기.kt
@@ -7,26 +7,34 @@ fun main() {
 
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
-    var n = br.readLine().toInt()
+    val n = br.readLine().toInt()
 
+    // 몇 번째 대각선인지
     var count = 1
+
+    // 지금까지 센 숫자 갯수
     var range = 1
 
-    while( n > range) {
-        count++
-        range += count
+    while(n > range) {
+        count++ // 다음 대각선으로
+        range += count // 대각선에서 센 숫자 추가 (k번째 대각선에 k개의 숫자 존재)
     }
 
+    // 대각선의 숫자 시작점
     val start = range - count + 1
+
+    // 대각선에서 n이 몇번째에 있는지
     val offset = n - start
 
-    var numerator = 0
-    var denominator = 0
+    var numerator = 0 // 분자
+    var denominator = 0 // 분모
 
+    // 대각선이 짝수번째면 분자가 1부터 
     if(count % 2 == 0) {
         numerator = offset + 1
         denominator = count - offset
     }
+    // 대각선이 홀수번째면 분모가 1부터
     else {
         numerator = count - offset
         denominator= offset + 1
